58fbeba56dc7badff769e9f1ac34366d4802b332
olegchursin <olegchursin@gmail.com>
2018-02-06 17:48:32 -0500

    Done.

--- src/list.js
+++ src/list.js
@@ -1,12 +1,22 @@
 /*
 list is responsible for creating a single list component
 */
+const listsArr = []
+
 const List = (() => {
-  let id = 1
+  let id = 0
   return class List {
     constructor(title) {
       //your code here
-      // NOTE: How can we use the private id variable to auto increment list ids🤔?
+      // Note: How can we use the private id variable to auto increment list ids?
+      //title, id, has many tasks
+      this.title = title
+      this.id = ++id
+      listsArr.push(this)
+    }
+
+    tasks() {
+      return tasksArr.filter(t => t.listId === this.id)
     }
   }
 
