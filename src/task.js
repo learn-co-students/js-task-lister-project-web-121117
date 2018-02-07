a4f56ab89c9b990093f8bc1c4cafd5141b99b852
github username <github email address>
2018-02-06 19:21:57 -0500

    Done.

--- src/task.js
+++ src/task.js
@@ -2,11 +2,23 @@
 task is responsible for creating a single task object
 */
 const Task = (() => {
-  let id = 1
+  let id = 1;
   return class Task {
     constructor(description, priority) {
-      //your code here
+      this.id = id++;
+      this.description = description;
+      this.priority = priority;
     }
-  }
 
-})()
+    render() {
+      let list = document.createElement("li");
+      list.append(`Task: ${this.description}`);
+      list.append(`Priority: ${this.priority}`);
+      let unordered = document.createElement("ul");
+      unordered.append(list);
+      lists.append(unordered);
+    }
+  };
+})();
+
+// jack was here
